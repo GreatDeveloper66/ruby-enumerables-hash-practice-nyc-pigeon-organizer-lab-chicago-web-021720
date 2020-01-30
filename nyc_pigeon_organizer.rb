05bb8850091a81175f095121aac1a50c4f36546c
cerebral-caller-0985 <ashaffe@gmail.com>
2020-01-30 17:04:12 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,17 +1,17 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   emptyhash = {}
-  colorhash = data[:color]
-  gender = data[:gender]
-  lives = data[:lives]
-  colorhash.each do |key,value|
-    value.each do |name|
-      if emptyhash[name]
-        emptyhash[name][:color].push(key.to_s)
-      else
-        emptyhash[name][:color] = [key.to_s]
-      end
-    end
-  end
+  #colorhash = data[:color]
+  #gender = data[:gender]
+  #lives = data[:lives]
+  #colorhash.each do |key,value|
+    #value.each do |name|
+      #if emptyhash[name]
+        #emptyhash[name][:color].push(key.to_s)
+      #else
+        #emptyhash[name][:color] = [key.to_s]
+      #end
+    #end
+  #end
   emptyhash
 end
